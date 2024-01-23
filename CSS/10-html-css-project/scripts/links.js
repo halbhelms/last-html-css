979c61320aa1e00f7929487bc42e81f3f0514ff7
Hal Helms <hal.helms@gmail.com>
2024-01-22 19:58:39 -0500

    WIP -- adding regions

--- CSS/10-html-css-project/scripts/links.js
+++ CSS/10-html-css-project/scripts/links.js
@@ -1,23 +1,17 @@
+// #region -- establish event listeners for links
+document.querySelectorAll('.nav-item').forEach(el => {
+  el.addEventListener('click', establishLink)
+})
+// #region -- event listener callback
 function establishLink(e) {
   e.preventDefault()
   loadContent(`./links/${e.target.dataset.link}.html`)
 }
-
+// #endregion
+// #region -- load content into main page
 function loadContent(url) {
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.text();
-    })
-    .then(html => {
+  fetch(url).then(html => {
       document.querySelector('div.overview > div.center').innerHTML = html;
     })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
 }
-document.querySelectorAll('.nav-item').forEach(el => {
-  el.addEventListener('click', establishLink)
-})
+// #endregion
